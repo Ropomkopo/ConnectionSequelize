7d56b26f5b1907bfbcb1ef0ef280414b3447042f
Roman <romko.lisenko@gmail.com>
2020-09-22 13:55:55 +0300

    Create connection to database use Sequelize

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const Sequelize = require('sequelize');
 
+const db = {};
+const sequelize = new Sequelize('mysql', 'root', 'Buster', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+db.sequelize.sync();
 
 io.on('connection', (socket) => {
     console.log('a user connected');
